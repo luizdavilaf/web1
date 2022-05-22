ff8ae1c7a063e54b445d394d8602219ab2ba8dfc
Luiz <luizdavilaf@gmail.com>
2022-05-22 04:25:01 -0300

    correcaooption

--- trabalho4/php/matricula/editar.php
+++ trabalho4/php/matricula/editar.php
@@ -13,6 +13,7 @@ $codaluno = $mat->getAluno()->getCod();
 $codCurso = $mat->getCurso()->getCod();
 ?>
 <html lang="pt-br">
+
 <head>
   <title>Editar</title>
   <meta charset="utf-8">
@@ -23,6 +24,7 @@ $codCurso = $mat->getCurso()->getCod();
   ?>
 </head>
 <div id="wrapper">
+
   <body class="body p-3 my-3 bg-dark text-white-50">
     <?php
     include_once('../../include/header.html');
@@ -31,53 +33,51 @@ $codCurso = $mat->getCurso()->getCod();
     <div class='tudo'>
       <h2> Editar Matriculas</h2>
 
-      <main class='container p-3 my-3 bg-dark text-white-50'>        
+      <main class='container p-3 my-3 bg-dark text-white-50'>
         <div class="form-container">
           <form action="./editar2.php" method="post">
             <div class="form-group">
               <label for="codigo">Código:</label>
               <input type="text" class="form-control" id="cod" name="cod" value="<?php echo $codmat; ?>" readonly>
             </div>
-            <div class="form-group">              
-            <div class='form-container'>
+            <div class="form-group">
+              <div class='form-container'>
                 Aluno:
                 <select name="aluno">
-                  <?php                  
-                  $alunos=$alunoDao->listaAlunos();
-                  foreach($alunos as $aluno){
+                  <?php
+                  $alunos = $alunoDao->listaAlunos();
+                  foreach ($alunos as $aluno) {
                     $codigoaluno = $aluno->getCod();
                     $nomealuno = $aluno->getNome();
-                    if($codigo==$codaluno){
+                    if ($codigoaluno == $codaluno) {
                       echo "<option selected value='$codigoaluno'> Código: $codigoaluno Nome: $nomealuno </option>";
-                    }
-                    else{
-                      echo "<option value='$codigoaluno'> Código: $codigoaluno Nome: $nomealuno </option>";                      
+                    } else {
+                      echo "<option value='$codigoaluno'> Código: $codigoaluno Nome: $nomealuno </option>";
                     }
                   }
                   ?>
                 </select>
               </div>
             </div>
-           
-           
+
+
             <div class="form-group">
               <div class='form-container'>
                 Curso:
                 <select name="curso">
                   <?php
-                  $cursos=$cursoDao->listaCurso();
-                  foreach($cursos as $curso){
+                  $cursos = $cursoDao->listaCurso();
+                  foreach ($cursos as $curso) {
                     $codigocurso = $curso->getCod();
                     $nometurma = $curso->getNomeTurma();
                     $sala = $curso->getSala();
                     $professor = $curso->getProfessor();
-                    if($codigocurso==$codcurso){
+                    if ($codigocurso == $codCurso) {
                       echo "<option selected value='$codigocurso'> Código: $codigocurso Turma: $nometurma
                       Sala: $sala Professor: $professor </option>";
-                    }
-                    else{
+                    } else {
                       echo "<option value='$codigocurso'> Código: $codigocurso Turma: $nometurma
-                      Sala: $sala Professor: $professor </option>";                     
+                      Sala: $sala Professor: $professor </option>";
                     }
                   }
                   ?>
@@ -98,4 +98,3 @@ $codCurso = $mat->getCurso()->getCod();
 </div>
 
 </html>
-
